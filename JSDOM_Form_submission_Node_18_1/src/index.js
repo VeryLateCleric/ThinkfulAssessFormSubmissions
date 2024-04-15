28d7903ae93ea49333b1d822dc09cf1d88a44643
Kenneth <kennethyoung.2960@gmail.com>
2024-04-15 01:52:00 -0500

    13 of 18 tests passing. Search Needs revision. Good night

--- JSDOM_Form_submission_Node_18_1/src/index.js
+++ JSDOM_Form_submission_Node_18_1/src/index.js
@@ -8,16 +8,16 @@ document.addEventListener("DOMContentLoaded", function () {
   searchForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    const searchTerm = searchTermInput.value.trim().toLowerCase()
+    const searchTerm = searchTermInput.value.trim();
     if (searchTerm === "") {
       displayError("Please enter a search term");
     } else {
       removeError();
       performSearch(searchTerm.toLowerCase());
     }
-  })
+  });
 
-// function for displaying any error messages
+  // function for displaying any error messages
 
   function displayError(message) {
     let errorElement = document.getElementById("searchError");
@@ -39,18 +39,21 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
+  // search function begins by hiding all and removing hidden tag if matching
   function performSearch(searchTerm) {
     const articles = document.querySelectorAll(".article");
+
     articles.forEach(function (article) {
-      const articleTitle = article.querySelector("h2").textContent.toLowerCase();
-      const isVisible = article.classList.contains("hidden");
+      article.classList.add("hidden");
+    });
+    articles.forEach(function (article) {
+      const articleTitle = article
+        .querySelector("h2")
+        .textContent.toLowerCase();
 
       if (articleTitle.includes(searchTerm)) {
         article.classList.remove("hidden");
-      } else {
-        article.classList.add("hidden")
       }
-    })
+    });
   }
-})
-  
+});
